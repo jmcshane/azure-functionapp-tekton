af70c65addc9494871944e1052b1c10c35c86f35
james.mcshane <james.mcshane@objectpartners.com>
2019-08-26 12:55:09 -0500

    Updating task to use env vars

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-mgmt"
-    storage_account_name = "droneterraformazure"
+    storage_account_name = "terraformmgmt"
     container_name       = "tfstate"
     key                  = "functionapp-demo.terraform.tfstate"
   }
