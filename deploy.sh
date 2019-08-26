33c547e0a6c294ffee8beee556a9d45deb21a97e
james.mcshane <james.mcshane@objectpartners.com>
2019-08-26 14:48:44 -0500

    Updating task

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 az login --service-principal -u ${USERNAME} -p ${PASSWORD} --tenant ${TENANT}
-func azure functionapp publish mcshane-tekton-functionapp-test
+func azure functionapp publish mcshane-tekton-functionapp-test --javascript
